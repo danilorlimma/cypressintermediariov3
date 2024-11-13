a79fd5010a02ac42e8205d607caa13a322677fea
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2024-11-13 00:04:58 -0300

    Atualiza commands

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './gui_commands.js'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
